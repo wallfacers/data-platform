2e4bb66fcf03681fa1ccbcd52fc6909d065bb38d
wallfacers <wallfacerswu@gmail.com>
2022-08-24 22:38:19 +0800

    'init'

---
+++ src/main/java/com/wallfacers/data/platform/DataPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.wallfacers.data.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPlatformApplication.class, args);
+	}
+
+}
